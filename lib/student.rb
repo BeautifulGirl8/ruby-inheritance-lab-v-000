ba35fe7a7f7ae8d4ec4b42b1b65a4a52e935eb80
Vicky  Tee <vttak9@gmail.com>
2021-01-28 07:33:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,16 +1,16 @@
-class Student
+
 class Student < User
 
-    def initialize
-        @knowledge = []
-    end
+  def initialize
+    @knowledge = []
+  end
 
-    def learn(str)
-        @knowledge << str
-    end
+  def learn(string)
+    @knowledge << string
+  end
 
-    def knowledge
-        @knowledge
-    end
-end
+  def knowledge 
+    @knowledge
+  end
+
 end 
